7567c0cdc8ff672b5355998a6f03f7cfa5553949
Regitaceza <128021793+Regitaceza@users.noreply.github.com>
2023-03-21 08:14:59 +0000

    Update Biodata

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  return "<p>Regita Ceza Dara Denanta</p>"
+  return "<p>Nama : Regita Ceza Dara Denanta </p><p>Tempat, tanggal lahir : Malang, 17 Desember 2003</p><p>Alamat : Ngembes, Modo, Lamongan</p><p>Jenis Kelamin : Perempuan</p><p>Agama : Islam</p>"
+
 
 
 if __name__ == "__main__":
